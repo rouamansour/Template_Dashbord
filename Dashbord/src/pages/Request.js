a8fa217bc1730de6bea4ae20dc9b6e4e55668871
rouamansour <rouamansour.rm@gmail.com>
2022-07-27 12:18:15 +0100

    delete and update

--- Dashbord/src/pages/Request.js
+++ Dashbord/src/pages/Request.js
@@ -1,15 +1,69 @@
-import React, { Component } from 'react'
+// import React,{Component} from 'react';
+// //import {Table} from 'react-bootstrap';
+// import {Button,ButtonToolbar} from 'react-bootstrap';
+// //import {AddRequest} from './AddRequest';
 
-export default class Request extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
+// export class Request extends Component{
 
-    render() {
+//     constructor(props){
+//         super(props);
+//         this.state={demds:[], addModalShow:false}
+//     }
 
-        return (
-            <h1> Hello  </h1>
-        );
-    }
-}
+//     refreshList(){
+//         fetch(process.env.REACT_APP_API+'Request')
+//         .then(response=>response.json())
+//         .then(data=>{
+//             this.setState({demds:data});
+//         });
+//     }
+
+//     componentDidMount(){
+//         this.refreshList();
+//     }
+
+//     // componentDidUpdate(){
+//     //     this.refreshList();
+//     // }
+
+//     render(){
+//         const {demds}=this.state;
+//         let addModalClose=()=>this.setState({addModalShow:false});
+//         return(
+//             <div >
+//                 <div className="mt-4" striped bordered hover size="sm">
+//                     <div>
+//                         <tr>
+//                             <th>Number</th>
+//                             <th>Comment</th>
+//                             <th>Title</th>
+//                             <th>Deadline</th>
+//                             <th>Type request</th>
+//                             <th>Options</th>
+//                         </tr>
+//                     </div>
+//                     <div>
+//                         {demds.map(dep=>
+//                             <tr key={dep.requestId}>
+//                                 <td>{dep.number}</td>
+//                                 <td>{dep.comment}</td>
+//                                 <td>{dep.title}</td>
+//                                 <td>{dep.deadline}</td>
+//                                 <td>{dep.typeRequestTypeId}</td>
+//                                 <td>Edit /Delete</td>
+//                             </tr>)}
+//                     </div>
+
+//                 </div>
+//                 {/* <ButtonToolbar>
+//                     <Button variant='primary'
+//                     onClick={()=>this.setState({addModalShow:true})}>
+//                     Add Request</Button>
+
+//                     <AddRequest show={this.state.addModalShow}
+//                     onHide={addModalClose}/>
+//                 </ButtonToolbar> */}
+//             </div>
+//         )
+//     }
+// }
--- Dashbord/src/routes.js
+++ Dashbord/src/routes.js
@@ -3,13 +3,11 @@ import { Navigate, useRoutes } from 'react-router-dom';
 import DashboardLayout from './layouts/dashboard';
 import LogoOnlyLayout from './layouts/LogoOnlyLayout';
 //
-import Blog from './pages/Blog';
 import User from './pages/User';
 import Login from './pages/Login';
 import NotFound from './pages/Page404';
 import Register from './pages/Register';
-import Products from './pages/Products'; 
-//import Request from './pages/Request';
+import Request from './pages/Request';
 import DashboardApp from './pages/DashboardApp';
 
 // ----------------------------------------------------------------------
@@ -22,9 +20,7 @@ export default function Router() {
       children: [
         { path: 'app', element: <DashboardApp /> },
         { path: 'user', element: <User /> },
-        // { path: 'Request', element: <Request /> },
-        { path: 'products', element: <Products /> },
-        { path: 'blog', element: <Blog /> },
+        { path: 'Request', element: <Request /> },
       ],
     },
     {
